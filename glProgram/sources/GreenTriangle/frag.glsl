05b74b5718c3d467a22ab37fc8987ec209d8e25c
Gáspár Róbert <gasparrobert0908@gmail.com>
2026-05-13 12:46:28 +0200

    added terrainColor

--- glProgram/sources/GreenTriangle/frag.glsl
+++ glProgram/sources/GreenTriangle/frag.glsl
@@ -6,7 +6,26 @@ in float vHeight;
 uniform vec3 color;
 out vec4 fragmentColor;
 
-void main(){
-	fragmentColor=vec4(color * vHeight,1.);
+const vec3 waterDeep=vec3(.04,.16,.38);
+const vec3 waterShallow=vec3(.10,.35,.58);
+const vec3 sand=vec3(.80,.74,.52);
+const vec3 grass=vec3(.22,.52,.18);
+const vec3 stone=vec3(.48,.45,.42);
+const vec3 snow=vec3(.92,.93,.97);
+
+const float seaLevel=.13;
+
+vec3 terrainColor(float h){
+    vec3 water=mix(waterDeep,waterShallow,smoothstep(seaLevel-.25,seaLevel,h));
+    
+    vec3 land=sand;
+    land=mix(land,grass,smoothstep(seaLevel+.04,seaLevel+.22,h));
+    land=mix(land,stone,smoothstep(.65,.80,h));
+    land=mix(land,snow,smoothstep(.82,.92,h));
+    
+    return mix(water,land,smoothstep(seaLevel-.02,seaLevel+.05,h));
 }
 
+void main(){
+    fragmentColor=vec4(terrainColor(vHeight)*color,1.);
+}
